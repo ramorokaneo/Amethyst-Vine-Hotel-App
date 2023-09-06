cadcd8d11f0d25e48e6fc727745d04c8e3e8ca39
Neo <ramoroka.neo@gmail.com>
2023-09-06 07:51:09 +0200

    main

--- frontend/amethyst-hotel/src/App.js
+++ frontend/amethyst-hotel/src/App.js
@@ -5,14 +5,17 @@ import Home from "./components/Page/Home/Home";
 import RoomList from "./components/Page/Room List/RoomList"
 import RoomDetail from "./components/Page/Room Detail/RoomDetail";
 import ContactUs from "./components/Pages/Contact/ContactUs";
-import Register from "./components/forms/Register";
-import LoginForm from "./components/forms/Login";
+import Signup from "./components/forms/Signup/Signup";
+import LoginForm from "./components/forms/Login/Login";
 import AdminRegisterForm from "./components/Admin/Forms/AdminRegisterForm";
+import Main from "./components/forms/Main/Main";
 
 
 
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
     <Router>
       <Routes>
@@ -21,8 +24,9 @@ function App() {
         <Route path="/rooms" element={<RoomList />} />
         <Route path="/rooms/:id" element={<RoomDetail />} />
         <Route path="/contact" element={<ContactUs/>} />
-        <Route path="/registration" element={<Register/>} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/Login" element={<LoginForm/>} />
+        {user && <Route path="/main" exact element={<Main />} />}
         <Route path="/admin-register" element={<AdminRegisterForm />} />
 
       </Routes>
